aa8cd4ff4ea1148de96b759ad15084e2983b84f5
BookHeart <87170360@qq.com>
2017-02-28 17:57:07 +0800

    add proto

--- hlddz/proto.h
+++ hlddz/proto.h
@@ -3,16 +3,26 @@
 
 enum client_command
 {
-    CLIENT_LOGIN                = 1001,   
-    CLIENT_LOGOUT               = 1002,
-    CLIENT_READY                = 1003,	 
+    CLIENT_LOGIN                = 1001,         //登录   
+    CLIENT_START                = 1002,         //开始 
+    CLIENT_CALL                 = 1003,         //叫分
+    CLIENT_FARMER               = 1004,         //农民加倍
+    CLIENT_OUT                  = 1005,         //出牌
 };
 
 enum server_command
 {
     SERVER_RESPOND              = 2000,         //客户端请求回复
-    SERVER_LOGIN                = 2001,
-    SERVER_LOGOUT               = 2002,
+    SERVER_PLAYER_INFO          = 2001,         //玩家信息
+    SERVER_CARD_1               = 2002,         //第一次发牌17张
+    SERVER_START_CALL           = 2003,         //开始叫分
+    SERVER_CALL_INFO            = 2004,         //叫分情况
+    SERVER_START_FARMER         = 2005,         //开始农民加倍
+    SERVER_FARMER_INFO          = 2006,         //农民加倍结果
+    SERVER_CARD_2               = 2007,         //第二次发牌3张底牌
+    SERVER_START_OUT            = 2008,         //开始出牌
+    SERVER_OUT_INFO             = 2009,         //出牌信息
+    SERVER_END                  = 2010,         //牌局结束
 };
 
 #endif
